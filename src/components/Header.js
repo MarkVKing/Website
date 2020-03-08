2c2e140ed8a92e035d50a7d741c47df9cd6f4eea
DillonSimeone <Dillonsimeone@gmail.com>
2020-03-08 08:12:38 -0700

    Inital Upload

---
+++ src/components/Header.js
@@ -0,0 +1,77 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import bookcover from '../images/bookCovers/1.jpg'
+import SEO from '../components/SEO'
+
+const Header = props => (
+  
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <SEO title="Home"></SEO>
+    <div className="logo">
+      <span className="icon fa-wheelchair"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <img src={bookcover} alt="Mark V King's book cover" />
+        <div className="description">
+          <h1>It Only Takes One</h1>
+          <p>
+            How one skiing accident changed a 16 year old boy's life forever.
+            However, through the ups and downs, this is not a tragedy, it's the
+            story of one man's remarkable perseverance and will to lead a
+            fulfilling life despite being given one hour to live. Thirty nine
+            years later, Mark V. King is an advocate for people with
+            disabilities and is one of the longest living ventilator dependent
+            c1 quadriplegics in the United States. This is his story.
+          </p>
+          <a href="https://www.amazon.com/Snow-Skis-Wheelchair-Quadriplegia-Failed/dp/108279838X">
+            <button>BUY AT AMAZON</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('aboutMe')
+            }}
+          >
+            Who I Am
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('resources')
+            }}
+          >
+            Resources
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+        <li>
+        <button onClick={() => {
+              props.onOpenArticle('blog')
+            }}>Blog</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
---
+++ src/components/Posts.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Disqus from 'disqus-react'
+import Parser from 'html-react-parser'
+import Sanitize from 'sanitize-html'
+
+// Alternatively, import specific members:
+// import { DiscussionEmbed, CommentCount, CommentEmbed } from 'disqus-react';
+const disqusShortname = 'markvking'
+
+const Posts = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allWordpressPost {
+          nodes {
+            content
+            excerpt
+            title
+            link
+          }
+        }
+      }
+    `}
+    render={data => {
+      const mostRecentPost = data.allWordpressPost.nodes[0]
+      let disqusConfig = {
+        identifier: mostRecentPost.link,
+        url: mostRecentPost.link,
+        title: mostRecentPost.title,
+        language: `en`,
+      }
+
+      function disqusReset(newIdentifier, newUrl, newTitle, newLanguage) {
+        //The leftovers of many attempts to get Disqus working... No luck. Disqus still refuse to put comments in their own threads based on the identifier. 
+        //UPDATE: I have no idea why this bit was not working until after I reset my computer... Each posts now gets their own comment sections!
+        //HERE BE DRAGONS - Don't change anything in this function! Just believe in the magic!
+        disqusConfig.shortname = disqusShortname
+        disqusConfig.identifier = newIdentifier
+        disqusConfig.url = newUrl
+        disqusConfig.title = newTitle
+        disqusConfig.language = newLanguage
+        console.log(window.DISQUS.reset)
+        window.DISQUS.reset({
+          reload: true,
+          config: function(){
+            this.page.identifier = disqusConfig.identifier
+            this.page.url = disqusConfig.url  
+          },
+        })
+      }
+
+      return (
+        <>
+          <div className="postsContainor noSelectOutline">
+            <div className="posts">
+              {data.allWordpressPost.nodes.map(post => (
+                <div
+                  key={post.wordpress_id}
+                  id={post.wordpress_id}
+                  className="post"
+                  style={{}}
+                  onClick={() => {
+                    disqusReset(
+                      post.link,
+                      post.link,
+                      post.title,
+                      `en`
+                    )
+                    document.getElementById('content').innerHTML = `<h1>` + post.title + '</h1>' + Sanitize(post.content,{
+                      allowedTags: Sanitize.defaults.allowedTags.concat([ 'img'])
+                    })
+                  }}
+                >
+                  <h4>{Parser(post.title)}</h4>
+                  <div className="excerpt">{Parser(post.excerpt)}</div>
+                </div>
+              ))}
+            </div>
+            <div id="mainPost">
+              <div id="content">
+                <h1>{Parser(data.allWordpressPost.nodes[0].title)}</h1>
+                {Parser(data.allWordpressPost.nodes[0].content)}
+              </div>
+              <div id="disqus_thread">
+                {/* <Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
+                        </Disqus.CommentCount> */}
+                <p>BLOGS GOES HERE - PEE IN PROGRESS - BLOGS GOES HERE</p>
+                <Disqus.DiscussionEmbed
+                  shortname={disqusShortname}
+                  config={disqusConfig}
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )
+    }}
+  />
+)
+
+export default Posts
